961d663e9767c1539f0452675213792e10c2b4d0
SashaPetelin <alexanderzr58@icloud.com>
2023-06-17 01:08:59 +0300

    .

--- src/main/java/Ex1.java
+++ src/main/java/Ex1.java
@@ -4,7 +4,9 @@ public class Ex1{
     }
 
     public static void division(double num1, double num2){
-        if(num2 ==  0) throw new ArithmeticException("Нельзя делить на 0");
+        if(num2 ==  0) {
+            throw new ArithmeticException("Нельзя делить на 0");
+        }
         double result = num1/num2;
         System.out.printf("Результат деления заданных чисел = " + result);
     }
